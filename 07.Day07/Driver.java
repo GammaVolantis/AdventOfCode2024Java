7789afb2d78b4fbf6dc2074977b2cf5c866b25c4
Staton Otto <statonotto@gmail.com>
2025-01-04 18:27:38 -0500

    Salted Day 7 with the test code.

--- 07.Day07/Driver.java
+++ 07.Day07/Driver.java
@@ -3,9 +3,25 @@ import java.util.ArrayList;
 
 class Driver{
     public static void main(String[] args) throws IOException {
-        //String file = "Test1.txt";
-        //ArrayList<String> data = Helper.FileReader(file);
+        //Test AnswerPt1 == 3749
+        String test1 = "Test1.txt";
+        String input = "Input.txt";
+        int test1Pt1 = HiddenMain(test1);
+        if(test1Pt1==3749){
+            System.out.println("Success!");
+        }
+        else{
+            System.out.println("Failure " + test1Pt1);
+        }
 
 
     }
+    
+    public static int HiddenMain(String file) throws IOException{
+        int finalScore = 0;
+        ArrayList<String> data = Helper.FileReader(file);
+
+        return finalScore;
+    }
+
 }
